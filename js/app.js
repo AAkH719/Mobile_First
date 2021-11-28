62da09fcb64f99e83aaf122ebb242273a9bb9162
AAkH719 <alinaahmedeeva@mail.ru>
2021-11-28 22:51:01 +0700

    Сделала кнопку больше туров

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
-// JavaScript source code
+let buttonToggleItems
+let listItemsContainer
+
+function init() {
+    buttonToggleItems = document.getElementsByClassName('more-block')
+    if(buttonToggleItems.length === 0) {
+      throw new Error('Кнопка не найдена в document');
+    }
+    
+    listItemsContainer
+    listItemsContainer = document.getElementsByClassName('tours')
+    
+    buttonToggleItems[0].addEventListener(
+      'click',
+      (event) =>
+        handleToggleItemsButtonClick(listItemsContainer[0], event)
+    )
+
+}
+
+window.addEventListener("DOMContentLoaded", init);
+
+function handleToggleItemsButtonClick(listItemsContainer, pointerEvent) {
+    const classForVisibleHiddenItems = 'block--hidden-visible';
+    
+    if(pointerEvent.currentTarget.innerText === 'Больше туров') {
+      pointerEvent.currentTarget.innerText = 'Меньше туров'
+      listItemsContainer.classList.add(classForVisibleHiddenItems)
+    } else {
+        pointerEvent.currentTarget.innerText = 'Больше туров'
+        listItemsContainer.classList.remove(classForVisibleHiddenItems)
+    }
+  }
+
